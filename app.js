5611e5fcd9d03acfddcef77299bfdbd1f0f45b70
acandael <info@anthonycandaele.com>
2020-01-17 15:30:06 +0100

    exercise status codes

--- app.js
+++ app.js
@@ -7,4 +7,10 @@ app.get('/', (req, res) => {
 	
 });
 
+app.get('/demo', (req, res) => {
+  res.set('X-full-stack', '4life');
+  res.status(418);
+  res.send('I prefer coffee');
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
